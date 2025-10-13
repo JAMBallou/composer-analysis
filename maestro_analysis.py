71d74f656e866045d35419f18c49d32e880a0b38
JAMBallou <jamballou@icloud.com>
2025-10-13 11:56:13 -0400

    Converted MAESTRO data to CSV format for easier reading.

--- maestro_analysis.py
+++ maestro_analysis.py
@@ -1,5 +1,7 @@
 import json
+import csv
 
+# load MAESTRO JSON data
 try:
     with open("maestro-v3.0.0.json", encoding="utf-8") as f:
         data = json.load(f)
@@ -13,7 +15,47 @@ except Exception as e:
     print(f"An unexpected error occurred: {e}")
 
 
+
+# count occurrences of each composer
 composers = list(data["canonical_composer"].values())
 composers_count = {i:composers.count(i) for i in composers}
-with open("composer_count.json", "w", encoding="utf-8") as f:  
-    json.dump(composers_count, f, indent=4)
+
+def json_to_csv(json_data, csv_filename, headers):
+    """Convert a JSON file to a CSV file."""
+    
+    with open(csv_filename, mode='w', newline='', encoding='utf-8-sig') as file:
+        writer = csv.writer(file)
+        
+        writer.writerow(headers)
+        writer.writerows(json_data.items())
+
+# sort composer_count by number of works
+sorted_composer_count = dict(sorted(composers_count.items(), key=lambda item: item[1], reverse=True))
+json_to_csv(sorted_composer_count, 'composer_count.csv', ['Composer', 'Count'])
+
+
+
+def columnar_json_to_csv(json_data, csv_filename):
+    # fetch indices
+    all_indices = set()
+    for column in json_data.values():
+        all_indices.update(column.keys())
+    # ensure sorted order
+    all_indices = sorted(all_indices, key=int)
+    
+    # fetch column headers
+    headers = list(json_data.keys())
+
+    # to CSV
+    with open(csv_filename, 'w', newline='', encoding='utf-8-sig') as f:
+        writer = csv.writer(f)
+        writer.writerow(['Index'] + headers)
+        
+        for idx in all_indices:
+            row = [idx]
+            for head in headers:
+                value = json_data.get(head, {}).get(idx, "")
+                row.append(value)
+            writer.writerow(row)   
+
+columnar_json_to_csv(data, 'maestro-v3.0.0.csv')
